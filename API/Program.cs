14f9dd8004f95fc1da002d0f39868b38962c4cf8
Oliver Tepper <oltepper@gmail.com>
2023-03-14 16:21:52 +0100

    Adding Authentication

--- API/Program.cs
+++ API/Program.cs
@@ -15,5 +15,6 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
 app.MapControllers();
 app.Run();
